66da052c3b738cdb40cf62200bcf9a238732f05b
Darpeex <foxyyysp@mail.ru>
2023-08-10 20:05:42 +0300

    feat: подключились к MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000, BASE_PATH } = process.env;
 const app = express();
 
-app.use('/', routes);
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log('Ссылка на сервер:');
