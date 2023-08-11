060156a3c660c1cbf043de33950c5a903ef7069d
Darpeex <foxyyysp@mail.ru>
2023-08-11 23:37:47 +0300

    feat: добавлены роуты пользователей, настроены в app.js

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const path = require('path');
+// const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const { PORT = 3000, BASE_PATH } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
+
+app.use(bodyParser.json()); // для собирания JSON-формата
+app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/cards', require('./routes/cards'));
+app.use('/', require('./routes/users'));
+
+// app.use(express.static(path.join(__dirname, 'public')));
 app.listen(PORT, () => {
   console.log('Ссылка на сервер:');
-  console.log(BASE_PATH);
+  console.log(`http://localhost:${PORT}/`);
 });
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const express = require('express');
+const router = require('express').Router();
 const { getUsers, getUser, createUser } = require('../controllers/users');
 
-const router = express.Router();
-
 router.get('/users', getUsers); // возвращает всех пользователей
-router.get('/users:userId', getUser); // возвращает пользователя по _id
+router.get('/users/:userId', getUser); // возвращает пользователя по _id
 router.post('/users', createUser); // создаёт пользователя
 
 module.exports = router;
