78f4e7017dce84882faf9b32992386931f95a92c
Darpeex <foxyyysp@mail.ru>
2023-08-12 16:04:33 +0300

    feat: добавлена обработка ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */ // убирает подчёркивание со стрелочной функции 'строка 33'
 /* eslint-disable no-unused-vars */ // убирает подчёркивание с неиспользуемых свойств
 const Card = require('../models/card');
 
@@ -15,7 +16,13 @@ module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link })
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(400).send({ message: err.message }));
+    .catch((err) => { // если введённые данные некорректны, возвращается ошибка с кодом '400'
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные пользователя' });
+      } else { // иначе, по-умолчанию, ошибка с кодом '500'
+        res.status(500).send({ message: err.message });
+      }
+    });
 };
 
 // удаляет карточку по идентификатору
@@ -23,20 +30,43 @@ module.exports.deleteCard = (req, res) => {
   const { id } = req.body;
 
   Card.deleteOne({ _id: id })
-    .then(() => res.send({ message: 'Карточка успешно удалена' }))
+    .then((result) => {
+      if (result.deletedCount === 0) { // Проверка удаления карточки
+        return res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      res.send({ message: 'Карточка успешно удалена' });
+    })
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 // поставить лайк карточке
-module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-  { new: true },
-);
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  )
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      res.send({ message: 'Лайк поставлен' });
+    })
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
 
 // убрать лайк с карточки
-module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $pull: { likes: req.user._id } }, // убрать _id из массива
-  { new: true },
-);
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  )
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      res.send({ message: 'Лайк удален' });
+    })
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
--- controllers/users.js
+++ controllers/users.js
@@ -25,26 +25,55 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => {
+      res.send({ data: user });
+    }).catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные пользователя' });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
 };
 
 // обновляет профиль
 module.exports.updateUserInfo = (req, res) => {
-  const { id } = req.user._id;
-  const updatedFields = req.body;
+  const { id } = req.user; // извлекаем id пользователя из объекта req.user
+  const updatedFields = req.body; // req.body содержит обновленные данные профиля пользователя
 
-  User.updateOne({ _id: id }, updatedFields)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+  User.findByIdAndUpdate(id, updatedFields) // находим пользователя по id и передаём новые данные
+    .then((user) => { // если обновление профиля выполнено успешно, выполнится след. блок
+      if (user) { // если возвращенное значение user не пустое, отправим клиенту новые данные
+        res.send({ data: user });
+      } else { // иначе, вернём ошибку с кодом '404' и сообщением 'пользователь не найден'
+        res.status(404).send({ message: 'Пользователь не найден' });
+      } // обработчик ошибок во время выполнения операции
+    }).catch((err) => { // если введённые данные некорректны, возвращается ошибка с кодом '400'
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные пользователя' });
+      } else { // иначе, по-умолчанию, ошибка с кодом '500'
+        res.status(500).send({ message: err.message });
+      }
+    });
 };
 
 // обновляет аватар
 module.exports.updateUserAvatar = (req, res) => {
   const { id } = req.user._id;
-  const updatedAvatar = req.body.avatar;
+  const updatedField = req.body.avatar;
 
-  User.updateOne({ _id: id }, updatedAvatar)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+  User.updateOne({ _id: id }, updatedField)
+    .then((avatar) => {
+      if (avatar) {
+        res.send({ data: avatar });
+      } else {
+        res.status(404).send({ message: 'Пользователь не найден' });
+      }
+    }).catch((err) => { // если введённые данные некорректны, возвращается ошибка с кодом '400'
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else { // иначе, по-умолчанию, ошибка с кодом '500'
+        res.status(500).send({ message: err.message });
+      }
+    });
 };
