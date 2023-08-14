ec740f8a22ec89ce77e3baf323691c34f854ad6a
Darpeex <foxyyysp@mail.ru>
2023-08-14 23:31:15 +0300

    chore: обновлены зависимости

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,11 @@
 module.exports = {
   env: {
-    browser: true,
+    browser: false,
     es2021: true,
   },
   extends: [
     'airbnb-base',
     'eslint:recommended',
-    'plugin:react/recommended',
   ],
   overrides: [
     {
@@ -25,9 +24,6 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
   rules: {
     'no-console': 'off',
     'no-underscore-dangle': ['error', { allow: ['_id'] }],
