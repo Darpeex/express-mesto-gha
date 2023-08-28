a2f1b782fd07ff1b42e2a888c2632877acb5d8b7
Darpeex <foxyyysp@mail.ru>
2023-08-28 15:43:26 +0300

    feat: добавлена проверка celevrate, Joi на уровне роутов

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,37 @@
-const router = require('express').Router(); // создание нового экземпляра маршрутизатора заместо app
+const { celebrate, Joi } = require('celebrate'); // библиотека для валидации данных
+
+const router = require('express').Router(); // создание нового экземпляра маршрутизатора вместо app
 const {
   getUsers, getUserById, getUserInfo, updateUserInfo, updateUserAvatar,
 } = require('../controllers/users');
 
-router.get('/users', getUsers); // возвращает всех пользователей
-router.get('/users/:userId', getUserById); // возвращает пользователя по _id
-router.get('/users/me', getUserInfo); // возвращает информацию о текущем пользователе
-router.patch('/users/me', updateUserInfo); // обновляет профиль
-router.patch('/users/me/avatar', updateUserAvatar); // обновляет аватар
+router.get('/users', celebrate({ // валидируем заголовки
+  headers: Joi.object().keys({ authorization: Joi.string().required() }).unknown(true),
+}), getUsers); // возвращает всех пользователей
+
+router.get('/users/:userId', celebrate({
+  headers: Joi.object().keys({ authorization: Joi.string().required() }).unknown(true),
+}), getUserById); // возвращает пользователя по _id
+
+router.get('/users/me', celebrate({
+  headers: Joi.object().keys({ authorization: Joi.string().required() }).unknown(true),
+}), getUserInfo); // возвращает информацию о текущем пользователе
+
+router.patch('/users/me', celebrate({
+  query: Joi.object({
+    name: Joi.string().default('Жак-Ив Кусто').min(2).max(30)
+      .required(),
+    about: Joi.string().default('Исследователь').min(2).max(30)
+      .required(),
+  }),
+}), updateUserInfo); // обновляет профиль
+
+router.patch('/users/me/avatar', celebrate({
+  query: Joi.object({
+    avatar: Joi.string().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png').uri()
+      .required(),
+  }),
+
+}), updateUserAvatar); // обновляет аватар
 
 module.exports = router;
