a1c12114b1a653a96072a8e65ef3021d272eeebf
Darpeex <foxyyysp@mail.ru>
2023-08-29 22:28:32 +0300

    feat: добавлена обработка роутов карточек

--- routes/cards.js
+++ routes/cards.js
@@ -5,28 +5,28 @@ const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
-router.get('/cards', celebrate({ // регистрируемся и создаём пользователя
-  body: Joi.object().keys({
+router.get('/cards', getCards); // возвращает все карточки
+router.post('/cards', celebrate({
+  body: Joi.object().keys({ // создаёт карточку
     name: Joi.string().min(2).max(30).required(),
     link: Joi.string().min(2).max(30).required()
       .pattern(/^(http|https):\/\/(www\.)?[a-zA-Z0-9\--._~:/?#[\]@!$&'()*+,;=]+#?$/),
   }),
-}), getCards); // возвращает все карточки
-router.post('/cards', createCard); // создаёт карточку
-router.delete('/cards/:cardId', celebrate({
+}), createCard);
+router.delete('/cards/:cardId', celebrate({ // удаляет карточку по идентификатору
   params: Joi.object().keys({
     userId: Joi.string().alphanum().length(24).required(),
   }),
-}), deleteCard); // удаляет карточку по идентификатору
-router.put('/cards/:cardId/likes', celebrate({
+}), deleteCard);
+router.put('/cards/:cardId/likes', celebrate({ // поставить лайк карточке
   params: Joi.object().keys({
     userId: Joi.string().alphanum().length(24).required(),
   }),
-}), likeCard); // поставить лайк карточке
-router.delete('/cards/:cardId/likes', celebrate({
+}), likeCard);
+router.delete('/cards/:cardId/likes', celebrate({ // убрать лайк с карточки
   params: Joi.object().keys({
     userId: Joi.string().alphanum().length(24).required(),
   }),
-}), dislikeCard); // убрать лайк с карточки
+}), dislikeCard);
 
 module.exports = router;
